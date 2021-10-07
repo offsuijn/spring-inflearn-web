8c37feb360b9ede760b8bc39f4730ef7554ee922
offsujin <offsujin@gmail.com>
2021-10-07 22:49:58 +0900

    Initial commit

---
+++ src/main/java/com/sujin/springhehe/SpringHeheApplication.java
@@ -0,0 +1,13 @@
+package com.sujin.springhehe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHeheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHeheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sujin/springhehe/SpringHeheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujin.springhehe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHeheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
