0b2fa9bd67228b9189c2eee99aebd4f0b1885f2a
offsujin <offsujin@gmail.com>
2021-10-08 14:03:49 +0900

    Add Domain, Repository

---
+++ src/main/java/com/sujin/springhehe/domain/Member.java
@@ -0,0 +1,23 @@
+package com.sujin.springhehe.domain;
+
+public class Member {
+
+    private long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
