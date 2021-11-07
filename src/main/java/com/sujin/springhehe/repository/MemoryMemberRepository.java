c0322517a666cae685086c8c7f6f8aceb6532abc
offsujin <offsujin@gmail.com>
2021-11-07 23:41:45 +0900

    Update Member Repository

--- src/main/java/com/sujin/springhehe/repository/MemoryMemberRepository.java
+++ src/main/java/com/sujin/springhehe/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package com.sujin.springhehe.repository;
 
 import com.sujin.springhehe.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
