167cf37debda18b2c2c5c99f2df9bcad5ff381c8
offsujin <offsujin@gmail.com>
2021-11-11 16:43:13 +0900

    Update Member Controller

--- src/main/java/com/sujin/springhehe/controller/MemberController.java
+++ src/main/java/com/sujin/springhehe/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.sujin.springhehe.controller;
 
+import com.sujin.springhehe.domain.Member;
 import com.sujin.springhehe.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class MemberController {
 
@@ -10,5 +12,13 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
     }
 }
