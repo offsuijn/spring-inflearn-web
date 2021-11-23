ab4708aac6818e786d8cbe6883f8769580ec67c2
offsujin <offsujin@gmail.com>
2021-11-23 15:48:46 +0900

    Change database to jdbc

--- src/main/java/com/sujin/springhehe/SpringConfig.java
+++ src/main/java/com/sujin/springhehe/SpringConfig.java
@@ -1,13 +1,21 @@
 package com.sujin.springhehe;
 
+import com.sujin.springhehe.repository.JdbcMemberRepository;
 import com.sujin.springhehe.repository.MemberRepository;
 import com.sujin.springhehe.repository.MemoryMemberRepository;
 import com.sujin.springhehe.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +23,7 @@ public class SpringConfig {
     }
 
     @Bean MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
