bfe5124ac6dc56f6014e16f33464447747e725ec
offsujin <offsujin@gmail.com>
2021-11-25 19:22:06 +0900

    Change database to Spring JPA

---
+++ src/main/java/com/sujin/springhehe/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,11 @@
+package com.sujin.springhehe.repository;
+
+import com.sujin.springhehe.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    Optional<Member> findByName(String name);
+}
